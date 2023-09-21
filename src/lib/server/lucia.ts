2ceaec221aac52ab7afe5d43a83fe67dbf5bc1f2
davidthus <dvdnicorici05@gmail.com>
2023-09-21 20:02:01 +0100

    initialise iziPM2

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,22 @@
+import { dev } from '$app/environment';
+import { prisma } from '@lucia-auth/adapter-prisma';
+import { PrismaClient } from '@prisma/client';
+import lucia from 'lucia-auth';
+import { sveltekit } from 'lucia-auth/middleware';
+
+const client = new PrismaClient();
+
+export const auth = lucia({
+	adapter: prisma(client),
+	env: dev ? 'DEV' : 'PROD',
+	middleware: sveltekit(),
+	transformDatabaseUser: (userData) => {
+		return {
+			userId: userData.id,
+			username: userData.username,
+			name: userData.name
+		};
+	}
+});
+
+export type Auth = typeof auth;
