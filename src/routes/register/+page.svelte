14084fa01a9a2b1f72abf5ddf7a2485e4f7e0441
davidthus <85802138+davidthus@users.noreply.github.com>
2023-09-25 09:42:30 +0100

    create routes/register form markup

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,19 @@
+<form method="post">
+
+  <hgroup>
+  <h2>Register</h2>
+  <h3>To create and join projects, you will need an account.</h3>
+  </hgroup>
+
+  <label for="username">Username</label>
+	<input type="text" id="username" name="username" required />
+
+	<label for="email">Email</label>
+	<input type="text" id="email" name="email" required />
+
+	<label for="password">Password</label>
+	<input type="password" id="password" name="password" required />
+
+	<button type="submit">Register</button>
+</form>
+<p>Already have an account? <a href="/login">Login</a></p>
